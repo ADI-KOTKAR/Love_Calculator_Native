03e2c6b3d1685f652ce53aacbee28aeefbeaa860
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-03-19 00:10:14 +0530

    Components in React Native

--- README.md
+++ README.md
@@ -6,3 +6,23 @@
 - https://github.com/expo/expo-cli/issues/134#issuecomment-443938890
 
 * JSX : similar to html, where JavaScript code is enclosed in '{}'.
+
+* Component : 
+    - Class based
+
+        const MyData = ()=>{
+            return (
+                <Text>Component</Text>
+            )
+        }
+
+
+    - Function based
+        
+        class MyData extends React.Component{
+            render(){
+                return(
+                    <Text>Component</Text>
+                )
+            }
+        }
--- amazingApp/App.js
+++ amazingApp/App.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button,Alert } from 'react-native';
 
-class App extends React.Component{
 
-   
+const MyData = ()=>{
+  return (
+    <Text>Component</Text>
+  )
+}
+
+// class MyData2 extends React.Component{
+//   render(){
+//     return(
+//       <Text>Component 2</Text>
+//     )
+//   }
+// }
+
+class App extends React.Component{
+  
   
   render(){
 
-    const diap = ['eat','sleep','code','repeat']
-    const list = diap.map(data=>{
-        return <Text> {data}</Text>
-      })
+    // const diap = ['eat','sleep','code','repeat']
+    // const list = diap.map(data=>{
+    //     return <Text> {data}</Text>
+    //   })
     return (
     <View style={styles.container}>
       <Text> {Math.random()} </Text>
-      {list}
       
-      <Button title="submit" />
+      <MyData/>
+       <Button title="submit" />
     </View>
     );
   }
