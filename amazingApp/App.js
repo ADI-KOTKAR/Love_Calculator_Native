1166757dec56d5025f5c0f1dc57dabcdb5dc95cb
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-03-19 03:39:59 +0530

    Scrool View in React Native

--- amazingApp/App.js
+++ amazingApp/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button,Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 
 class App extends React.Component{
 
-   
+   state={
+     text:"loading"
+   }
   
   render(){
 
     return (
     <View style={styles.container}>
-      <Text style={{fontSize:30,backgroundColor:'red'}} >Hello</Text>
+      
+      <ScrollView>
+        <Image source={{uri:"https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",height:400,width:300}}/>
+        <Image source={{uri:"https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",height:400,width:300}}/>
+        <Image source={{uri:"https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",height:400,width:300}}/>
+        <Image source={{uri:"https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",height:400,width:300}}/>
+        <Image source={{uri:"https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",height:400,width:300}}/>
+        <Image source={{uri:"https://images.unsplash.com/photo-1558980395-be8a5fcb4251?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",height:400,width:300}}/>
 
+      </ScrollView>
+      <Text style={{fontSize:30}} >Hello</Text>
     </View>
     );
   }
@@ -27,6 +38,6 @@ const styles = StyleSheet.create({
     flexDirection : 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor:"yellow"
+    
   }
 });
